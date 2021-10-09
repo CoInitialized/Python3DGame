a5c078bb16ea6698045fd9d4ec5e0daa43cfe9e2
damiankucharski <c.damiankucharski@gmail.com>
2021-10-09 15:50:40 +0200

    Cudowna gra minkraft

--- main.py
+++ main.py
@@ -4,12 +4,45 @@ import FirstPersonController
 
 from random import uniform
 
-app = Ursina()
-ground = Entity(model='plane',
-                texture = 'grass',
-                collider = 'mesh',
-                scale= (100,1,100))
 
-player = FirstPersonController()
-Sky()
-app.run()
+if __name__ == "__main__":
+
+
+    app = Ursina()
+    Sky()
+    player = FirstPersonController()
+
+    boxes  = []
+    for n in range(-8,8):
+        for k in range(-8,8):
+            box = Button(
+                parent = scene,
+                model='cube',
+                origin_y = 0.5,
+                texture = 'white_cube',
+                color=color.orange,
+                highlight_color = color.lime,
+                position=(k,0,n)
+            )
+            boxes.append(box)
+
+    def input(key):
+        for box in boxes:
+            if box.hovered:
+                if key=="left mouse down":
+                    boxes.append(
+                        Button(
+                            parent=scene,
+                            model='cube',
+                            origin_y=0.5,
+                            texture='white_cube',
+                            color=color.orange,
+                            highlight_color=color.lime,
+                            position=mouse.normal + box.position
+                        )
+                    )
+                if key=='right mouse down':
+                    boxes.remove(box)
+                    destroy(box)
+
+    app.run()
